ea2928e6db98e45aec3673a26b76935ed7a6a326
JeffJacobson <jeffrey.m.jacobson@gmail.com>
2013-02-12 17:25:41 -0800

    Added batch file to create web.config by copying sample.

---
+++ CreateWebConfig.cmd
@@ -0,0 +1,4 @@
+@echo off
+REM Copies the Sample.Web.Config to create Web.Config file. This is necessary since the Web.Config file is not stored in the repository.
+@echo Creating Web.Config...
+copy Sample.Web.Config Web.Config
